b284d545f61ba1b07cfd47c41489eed612583e1e
Alexey Ermakov <zee@technocore.ru>
2012-08-10 18:11:44 +0400

    Show unique versions in versions column

--- src/main/java/org/tomfolga/PomModulesVersionColumn.java
+++ src/main/java/org/tomfolga/PomModulesVersionColumn.java
@@ -1,24 +1,29 @@
 package org.tomfolga;
 
 import hudson.Extension;
+import hudson.maven.MavenModule;
+import hudson.maven.MavenModuleSetBuild;
 import hudson.model.Descriptor;
 import hudson.views.ListViewColumn;
 import net.sf.json.JSONObject;
-
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.StaplerRequest;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
- * 
+ *
  * This class defines a column that shows Maven POM Modules groupId, artifactId
  * and version.
- * 
+ *
  * This class has multiple descriptors which instantiate this class with
  * different params. I am not sure if it's recommended way in Hudson, but it
  * allows to create multiple column definition using one class/jelly file. TODO:
  * check if/how columns can be configured to avoid multiple columns
- * 
+ *
  * @author tomfolga
- * 
+ *
  */
 public class PomModulesVersionColumn extends AbstractPomModulesColumn {
 
@@ -30,6 +35,19 @@ public class PomModulesVersionColumn extends AbstractPomModulesColumn {
 
 	public PomModulesVersionColumn(String columnName) {
 		super(columnName, false, false, true);
+	}
+
+	public String uniqueVersionsString(Object lastBuild) {
+		SortedSet<String> result = new TreeSet<String>();
+
+		if (lastBuild instanceof MavenModuleSetBuild) {
+			MavenModuleSetBuild moduleSetBuild = (MavenModuleSetBuild) lastBuild;
+			for (MavenModule module : moduleSetBuild.getModuleLastBuilds().keySet()) {
+				result.add(module.getVersion());
+			}
+		}
+
+		return StringUtils.join(result, ", ");
 	}
 
 	@Extension
